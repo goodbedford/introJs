c131dcb7a2141f4cc9c783dac848d49ed5e64c58
goodbedford <bedford@goodbedford.com>
2016-04-01 23:42:53 -0700

    i done a lot

---
+++ js/app.js
@@ -0,0 +1,58 @@
+(function() {
+    "use strict";
+
+
+    active();
+    function active() {
+        //addBorderStyle();
+        addMenuHandler();
+        addQuizHandeler();
+    }
+
+    function addBorderStyle() {
+        var $infoPanels = $(".info-panel");
+
+        console.log($infoPanels);
+
+        $infoPanels.each(handlePanels);
+
+        function handlePanels (index) {
+
+            var $panel = $(this);
+            if (index % 2 === 0){
+                //even
+                $panel.addClass("info-panel-border-even");
+            } else {
+                $panel.addClass("info-panel-border-odd");
+            }
+            return this;
+        }
+    }
+
+    function addMenuHandler() {
+        var $btnMenu = $("#menu-btn");
+
+        $btnMenu.on("click", handleMenu)
+
+        function handleMenu(event) {
+            event.preventDefault();
+            var $menuWrapper = $("#menu-wrapper");
+            var $mainWrapper = $("#main-wrapper");
+            $menuWrapper.toggle();
+            $mainWrapper.toggleClass("col-xs-offset-4");
+        }
+    }
+
+    function addQuizHandeler() {
+        var $btnAnswer = $(".btn-answer");
+
+        $btnAnswer.on("click", handleAnswer);
+
+        function handleAnswer() {
+            var $parent = $(this).parent();
+            var $answerPanel = $parent.find(".answer-panel-body").first();
+            $answerPanel.toggleClass("hidden");
+
+        }
+    }
+})();
