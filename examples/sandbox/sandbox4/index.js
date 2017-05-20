e60b185ebf7926487373ff807cec83605b5ee215
goodbedford <bedford@goodbedford.com>
2017-05-20 09:57:36 -0700

    update examples

---
+++ examples/sandbox/sandbox4/index.js
@@ -0,0 +1,10 @@
+console.log("hi, i am working");
+
+//i am a comment
+
+let a = 5;
+let b = 6;
+
+console.log(a+b);
+
+console.log(a)
---
+++ examples/sandbox/sandbox5/js/index.js
@@ -0,0 +1,19 @@
+//what to wear
+
+const ITEMS = {
+	hatCold: "images/hat-cold.jpeg",
+	hatSun: "images/hat-sun.jpeg",
+	jacket: "images/jacket.jpeg",
+	pantsMen: "images/pants-men.jpeg",
+	pantsWomen: "images/pants-women.jpeg",
+	shirtLong: :"images/shirt-long.jpeg",
+	tShirt: "images/t-shirt.jpeg",
+	shorts: "images/shorts.jpeg"
+}
+
+function whatToWear(temp) {
+
+	return items;
+}
+// let pic = document.createElement("img");
+// pic.src = ITEMS.hat;
